f8e9d55055f7fc59c71134aea3f4743110d7fb57
DaniloNogueira-Silva <danilo.nogueira1802@gmail.com>
2025-08-27 18:53:37 -0300

    refactor: removing opentelemetry code

--- apps/orders/src/main.ts
+++ apps/orders/src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { OrdersModule } from './orders.module';
 import { ValidationPipe } from '@nestjs/common';
-import { initTracing } from './instrument';
 
 async function bootstrap() {
-  await initTracing();
-
   const app = await NestFactory.create(OrdersModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.port ?? 3001);
